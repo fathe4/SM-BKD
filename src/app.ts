8b0774f58874a85f96c019b6dfc2fbe43d45bf48
f.karim <f.karim@pythys.com>
2025-08-30 02:50:37 +0600

    Modifed chat service for better system design

--- src/app.ts
+++ src/app.ts
@@ -67,6 +67,11 @@ app.use(
     contentSecurityPolicy: false,
   })
 );
+app.post(
+  `${apiPrefix}/payments/webhook`,
+  express.raw({ type: "application/json" }),
+  handleStripeWebhookController
+);
 
 app.use(`${apiPrefix}/payments`, paymentRoutes);
 app.use(express.json()); // Parse JSON request bodies
