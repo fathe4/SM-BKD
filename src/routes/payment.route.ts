41cf8da813654ec5be388bdd21ab6e63d74e0a41
f.karim <f.karim@pythys.com>
2025-08-30 02:50:54 +0600

    Modifed chat service for better system design

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -13,11 +13,11 @@ const router = Router();
  * @desc    Stripe webhook handler
  * @access  Public
  */
-router.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  handleStripeWebhookController
-);
+// router.post(
+//   "/webhook",
+//   express.raw({ type: "application/json" }),
+//   handleStripeWebhookController
+// );
 
 router.use(express.json());
 
